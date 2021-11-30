830ef3b50ba9912ad58c8b44e888fd56f8c34c1f
DangHung301 <hung301le@gmail.com>
2021-11-30 23:14:35 +0700

    bottom navigation

--- app/src/main/java/com/example/apptruyen/MainActivity.kt
+++ app/src/main/java/com/example/apptruyen/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.apptruyen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var navigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+//        navigation.setOnItemSelectedListener {
+//
+//
+//            return@setOnItemSelectedListener true
+//        }
+//        navigation.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
+//            @Override
+//            fun onNavigationOtemSelected() : Boolean {
+//
+//                return true
+//            }
+//        })
     }
 }
